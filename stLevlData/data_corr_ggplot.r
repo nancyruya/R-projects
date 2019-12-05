2975b632fd730bc5a0cd73c91656bdd1edebad45
nancyruya <34261425+nancyruya@users.noreply.github.com>
2019-12-05 16:16:22 -0500

    Update data_corr_ggplot.r

--- stLevlData/data_corr_ggplot.r
+++ stLevlData/data_corr_ggplot.r
@@ -9,3 +9,10 @@ ggplot(dat1, aes(x=ATFRegWeapon, y=AllMortAdj)) +
   geom_text(aes(label=''),hjust=0, vjust=0) +
   facet_grid(.~ 1)+
   geom_smooth(method=lm, se=FALSE, fullrange=TRUE)
+
+# ggplot(dat1, aes(x=ATFRegWeapon, y=AllMortAdj)) +
+ # geom_point(size=3, shape=1, color='red')+
+ # geom_text(aes(label='State'),hjust=0, vjust=0) +
+ # facet_grid(.~ 'state level data')+
+ # geom_smooth(method=lm, se=FALSE, fullrange=TRUE) +
+ # theme(strip.text = element_text(size=15))
