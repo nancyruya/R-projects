57e01c7486d2cc4afa803da7379b05b00f49a973
nancyruya <34261425+nancyruya@users.noreply.github.com>
2019-12-30 20:59:43 -0500

    Update data_corr_ggplot.r

--- stLevlData/data_corr_ggplot.r
+++ stLevlData/data_corr_ggplot.r
@@ -23,6 +23,27 @@ fit <- lm(AllMortAdj ~ ATFRegWeapon + Poverty + Unemploy + PoorMH0 + PoorMH14ls 
 summary(fit)
 
 # Multiple regression model with two continuous predictor variables with or without interaction
-fit3=lm(AllMortAdj~ATFRegWeapon*Poverty,data=dat1)
-summary(fit3)
+fit1=lm(AllMortAdj~ATFRegWeapon*Poverty,data=dat1)
+summary(fit1)
 ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=Poverty))+geom_point()+stat_smooth(method="lm",se=FALSE)
+
+fit2=lm(AllMortAdj~ATFRegWeapon*Unemploy,data=dat1)
+summary(fit2)
+ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=Unemploy))+geom_point()+stat_smooth(method="lm",se=FALSE)
+
+fit3=lm(AllMortAdj~ATFRegWeapon*PoorMH0,data=dat1)
+summary(fit3)
+ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=PoorMH0))+geom_point()+stat_smooth(method="lm",se=FALSE)
+
+fit4=lm(AllMortAdj~ATFRegWeapon*PoorMH14ls,data=dat1)
+summary(fit4)
+ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=PoorMH14ls))+geom_point()+stat_smooth(method="lm",se=FALSE)
+
+fit5=lm(AllMortAdj~ATFRegWeapon*PoorMH14gt,data=dat1)
+summary(fit5)
+ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=PoorMH14gt))+geom_point()+stat_smooth(method="lm",se=FALSE)
+
+fit6=lm(AllMortAdj~ATFRegWeapon*StPoP,data=dat1)
+summary(fit6)
+ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=StPoP))+geom_point()+stat_smooth(method="lm",se=FALSE)
+
