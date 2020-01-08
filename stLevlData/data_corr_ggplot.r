cae99490a7f8b2c99a3439248d63570fddf265f9
nancyruya <34261425+nancyruya@users.noreply.github.com>
2020-01-08 18:42:54 -0500

    Update data_corr_ggplot.r

--- stLevlData/data_corr_ggplot.r
+++ stLevlData/data_corr_ggplot.r
@@ -52,3 +52,43 @@ Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’
 Residual standard error: 2.805 on 42 degrees of freedom
 Multiple R-squared:  0.5815,	Adjusted R-squared:  0.5118 
 F-statistic: 8.339 on 7 and 42 DF,  p-value: 2.356e-06
+
+# Global Null Hypothesis
+# When testing the null hypothesis that there is linear association between All Firearm Mortality, ATF Registered Weapons,
+# poverty, unemployment, Poor Mental Health 0 days, poor mental health 1-14 days, poor mental health >14 days, statewide population.
+# explain 58.15% of the variability in All Firearm Mortality.
+# Significant p-value: ATFRegWeapon (p-value = 0.189), Poverty (p-value = 0.013), StPop (p-value = 0.012)
+
+
+> fit <- lm(AllMortAdj ~ ATFRegWeapon + Poverty + StPoP, data=dat1)
+> summary(fit)
+
+Call:
+lm(formula = AllMortAdj ~ ATFRegWeapon + Poverty + StPoP, data = dat1)
+
+Residuals:
+    Min      1Q  Median      3Q     Max 
+-6.7416 -1.4802 -0.2369  1.1582 10.4254 
+
+Coefficients:
+               Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  -7.856e-02  1.867e+00  -0.042  0.96662    
+ATFRegWeapon  3.324e-05  9.762e-06   3.405  0.00138 ** 
+Poverty       8.287e-01  1.304e-01   6.357 8.44e-08 ***
+StPoP        -4.457e-07  9.709e-08  -4.590 3.42e-05 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 2.723 on 46 degrees of freedom
+Multiple R-squared:  0.568,	Adjusted R-squared:  0.5398 
+F-statistic: 20.16 on 3 and 46 DF,  p-value: 1.74e-08
+
+# Global Null Hypothesis
+# When testing the null hypothesis that there is linear association between All Firearm Mortality, ATF Registered Weapons,
+# Poverty, and statewide population. ATF Registered weapons, poverty, and statewide polulation explain 56.8% of the
+# variability in All Firearm Mortality.
+# Main Effects Hypothesis
+# When testing the null hypothesis that there is linear association between All Firearm Morality and ATF Registered Weapons
+# after adjusting for poverty and statewide population, we reject the null hypothesis (t = 3.405, df = 46, p-value = 0.138).
+# for a one unit change in ATF Registered Weapons, on average, the All Firearm Mortality increases by 3.3e-05, after adjusting
+# for poverty and statewide population.
