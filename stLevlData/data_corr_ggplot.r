21ed0cf9ef1eebc8d0e0e08d8fe4636e4f0b70d7
nancyruya <34261425+nancyruya@users.noreply.github.com>
2020-01-08 17:34:33 -0500

    Update data_corr_ggplot.r

--- stLevlData/data_corr_ggplot.r
+++ stLevlData/data_corr_ggplot.r
@@ -19,31 +19,36 @@ ggplot(dat1, aes(x=ATFRegWeapon, y=AllMortAdj)) +
 
  #r2 = round(summary(x)$r.squared, digits = 2)
 
-fit <- lm(AllMortAdj ~ ATFRegWeapon + Poverty + Unemploy + PoorMH0 + PoorMH14ls + PoorMH14gt + StPoP, data=dat1)
-summary(fit)
 
 # Multiple regression model with two continuous predictor variables with or without interaction
 fit1=lm(AllMortAdj~ATFRegWeapon*Poverty,data=dat1)
 summary(fit1)
 ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=Poverty))+geom_point()+stat_smooth(method="lm",se=FALSE)
 
-fit2=lm(AllMortAdj~ATFRegWeapon*Unemploy,data=dat1)
-summary(fit2)
-ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=Unemploy))+geom_point()+stat_smooth(method="lm",se=FALSE)
+> fit <- lm(AllMortAdj ~ ATFRegWeapon + Poverty + Unemploy + PoorMH0 + PoorMH14ls + PoorMH14gt + StPoP, data=dat1)
+> summary(fit)
 
-fit3=lm(AllMortAdj~ATFRegWeapon*PoorMH0,data=dat1)
-summary(fit3)
-ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=PoorMH0))+geom_point()+stat_smooth(method="lm",se=FALSE)
+Call:
+lm(formula = AllMortAdj ~ ATFRegWeapon + Poverty + Unemploy + 
+    PoorMH0 + PoorMH14ls + PoorMH14gt + StPoP, data = dat1)
 
-fit4=lm(AllMortAdj~ATFRegWeapon*PoorMH14ls,data=dat1)
-summary(fit4)
-ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=PoorMH14ls))+geom_point()+stat_smooth(method="lm",se=FALSE)
+Residuals:
+    Min      1Q  Median      3Q     Max 
+-7.1602 -1.4565 -0.0834  0.9936 10.1491 
 
-fit5=lm(AllMortAdj~ATFRegWeapon*PoorMH14gt,data=dat1)
-summary(fit5)
-ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=PoorMH14gt))+geom_point()+stat_smooth(method="lm",se=FALSE)
+Coefficients:
+               Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  -7.498e+03  8.313e+03  -0.902 0.372207    
+ATFRegWeapon  3.471e-05  1.047e-05   3.315 0.001894 ** 
+Poverty       8.438e-01  2.000e-01   4.220 0.000128 ***
+Unemploy      7.654e-02  4.168e-01   0.184 0.855184    
+PoorMH0       7.493e+01  8.312e+01   0.901 0.372465    
+PoorMH14ls    7.513e+01  8.317e+01   0.903 0.371533    
+PoorMH14gt    7.500e+01  8.312e+01   0.902 0.372058    
+StPoP        -4.724e-07  1.115e-07  -4.236 0.000121 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-fit6=lm(AllMortAdj~ATFRegWeapon*StPoP,data=dat1)
-summary(fit6)
-ggplot(dat1,aes(y=AllMortAdj,x=ATFRegWeapon,color=StPoP))+geom_point()+stat_smooth(method="lm",se=FALSE)
-
+Residual standard error: 2.805 on 42 degrees of freedom
+Multiple R-squared:  0.5815,	Adjusted R-squared:  0.5118 
+F-statistic: 8.339 on 7 and 42 DF,  p-value: 2.356e-06
