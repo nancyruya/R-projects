dd208a9d6c1eed0d5a4791a7bfdc575ee1a7885c
RuyaFlute <34261425+nancyruya@users.noreply.github.com>
2021-05-03 19:12:01 -0400

    final aggregation 2018

--- Homebased_care/API/homecare.R
+++ Homebased_care/API/homecare.R
@@ -47,6 +47,28 @@ HCPCS18$year <- "2018"
 HCPCStxt18 <- as.data.frame(aggregate(NumberServices ~ HDescription, dat0a, sum))
 HCPCStxt18$year <- "2018"
 
+dat0a$HCPCS3 <- ifelse(substr(dat0a$HCPCS,1,1)=="G", 
+                       substr(dat0a$HCPCS,1,4), substr(dat0a$HCPCS,1,3))
+head(dat0a$HCPCS3)
+tail(dat0a$HCPCS3)
+length(unique(dat0a$HCPCS3))
+HCPCS318 <- as.data.frame(aggregate(NumberServices ~ HCPCS3, dat0a, sum))
+HCPCS318$year <- "2018"
 
+dat0a$HCPCSgp <- ifelse(dat0a$HCPCS3 %in% c("908"), "Psych related services",
+                        ifelse(dat0a$HCPCS3 %in% c("110"), "Paring or cutting of benign hyperkeratotic lesion",
+                               ifelse(dat0a$HCPCS %in% c("11719"), "Trimming of nondystrophic nails",
+                                      ifelse(dat0a$HCPCS %in% c("99341","99342","99343","99344", "99345"), "New patient home visit",
+                                             ifelse(dat0a$HCPCS %in% c("99347","99348","99349","99350"), "Established patient home visit",
+                                                    ifelse(dat0a$HCPCS %in% c("99324","99325", "99326", "99327", "99328"), "New Patient domiciliary, rest home, or custodial care visit",
+                                                           ifelse(dat0a$HCPCS %in% c("99334","99335", "99336", "99337"), "Established patient domiciliary, rest home, or custodial care visit",
+                                                                  ifelse(dat0a$HCPCS %in% c("11720","11721"), "Debridement of nails",
+                                                                         ifelse(dat0a$HCPCS3 %in% c("G012"), "Trimming of dystrophic nails", "Telehealt con ccare"                 
+                                                                                                     )))))))))
+head(dat0a$HCPCSgp)
+tail(dat0a$HCPCSgp)
+length(unique(dat0a$HCPCSgp))
+HCPCSgp18 <- as.data.frame(aggregate(NumberServices ~ HCPCSgp, dat0a, sum))
+HCPCSgp18$year <- "2018"
 
 
